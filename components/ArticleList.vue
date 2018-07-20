5624a9ac4d5e7f7820c0708dda0d184e1dc7e88d
keisukekonno <konnokeisuke@gmail.com>
2018-07-20 11:24:52 +0900

    feat 記事一覧コンポーネントの作成、定義

---
+++ components/ArticleList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>記事一覧</h1>
+    <div v-for="post in posts" :key="post.id">
+      タイトル：{{ post.fields.title }}
+      本文：{{ post.fields.main_content }}
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: ['posts']
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,45 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-netlify-contentful
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <ArticleList :posts="posts"></ArticleList>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  // 記事一覧コンポーネントを呼び出し
+  import ArticleList from '~/components/ArticleList.vue'
 
-export default {
-  components: {
-    AppLogo
+  // contentfulの宣言
+  import {createClient} from '~/plugins/contentful.js'
+  // 設定情報の取得
+  const client = createClient()
+
+  export default {
+    // 宣言したコンポーネントをindex.vueで使用する為に定義
+    components: {
+      ArticleList
+    },
+    // 変数の宣言
+    data () {
+      return {
+        posts: []
+      }
+    },
+    // データの取得(非同期)
+    asyncData ({ env }) {
+      // contentfulより記事データを取得
+      return client.getEntries({
+        // 対象のブログID
+        'content_type': env.CTF_BLOG_POST_TYPE_ID,
+        // 最大取得件数
+        'limit': 10,
+      }).then(entries => {
+        // 取得出来たのでindex.vueに返却
+        return {
+          posts: entries.items
+        }
+      }).catch(console.error)
+    }
   }
-}
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
